86fb76d5ddb036c87584eca94e9b7b7863861b52
Maksim Titov <maksim_titov@epam.com>
2016-09-12 22:37:19 +0300

    developed business objects: user and letter

--- WebDriver_Part1/BusinessObjects/Letter.cs
+++ WebDriver_Part1/BusinessObjects/Letter.cs
@@ -3,11 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebDriver_Part1.Utils;
 
 namespace WebDriver_Part1.BusinessObjects
 {
     class Letter
     {
+        private const string _defaultMail = "default";
+        private string _to;
+        private string _subject;
+        private string _body;
 
+        public Letter(string letterName = _defaultMail)
+        {
+            string[] letterFields = ConfigParser.ParseLetter(letterName);
+            Addressee = letterFields[0];
+            Subject = letterFields[1];
+            Body = letterFields[2];
+        }
+
+        public string Addressee
+        {
+            private set { _to = value; }
+            get { return _to; }
+        }
+
+        public string Subject
+        {
+            private set { _subject = value; }
+            get { return _subject; }
+        }
+
+        public string Body
+        {
+            private set { _body = value; }
+            get { return _body; }
+        }
     }
 }
--- WebDriver_Part1/BusinessObjects/User.cs
+++ WebDriver_Part1/BusinessObjects/User.cs
@@ -3,11 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebDriver_Part1.Utils;
 
 namespace WebDriver_Part1.BusinessObjects
 {
     class User
     {
+        private const string _defaultUser = "default";
+        private string _login;
+        private string _password;
+        private string _domain;
+        
+        public User(string username = _defaultUser)
+        {
+            string[] userFields = ConfigParser.ParseUser(username);
+            Login = userFields[0];
+            Password = userFields[1];
+            Domain = userFields[2];
+        }
 
+        public string Login
+        {
+            private set { _login = value; }
+            get { return _login; }
+        }
+
+        public string Password
+        {
+            private set { _password = value; }
+            get { return _password; }
+        }
+
+        public string Domain
+        {
+            private set { _domain = value; }
+            get { return _domain; }
+        }
     }
 }
