ffce77a52efab0717bbe6bc0ee71a97ff48877af
Maksim Titov <maksim_titov@epam.com>
2016-09-12 22:40:52 +0300

    replaced constants with business objects; completed task 1: TAF Basics

--- WebDriver_Part1/tests/TestBusinessObjects.cs
+++ WebDriver_Part1/tests/TestBusinessObjects.cs
@@ -10,10 +10,6 @@ namespace WebDriver_Part1.tests
     [TestClass]
     public class TestBusinessObjects
     {
-        private const string toEmail = "docent.86@mail.ru";
-        private const string subjEmail = "Hello from webdriver";
-        private const string bodyEmail = "Hello!!!\n\rThis email is sent automatically by selenium WebDriver!\n\rBest regards,\n\rSelenuim WebDriver.";
-
         IWebDriver driver;
 
         [TestMethod]
@@ -32,14 +28,14 @@ namespace WebDriver_Part1.tests
             //No any other waits handled this, only hardcoded wait
             System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
             DraftsPage draftpage = homepage.GoToDraftsFolder();
-            draftpage.OpenSavedDraft(bodyEmail);
-            Assert.IsTrue(draftpage.IsElementPresent(By.XPath("//span[text() = '" + toEmail + "']")), "Draft email was not saved");
-            Assert.IsTrue(draftpage.CheckDraftContent(toEmail, bodyEmail), "Error in draft content");
+            draftpage.OpenSavedDraft(letter.Body);
+            Assert.IsTrue(draftpage.IsElementPresent(By.XPath("//span[text() = '" + letter.Addressee + "']")), "Draft email was not saved");
+            Assert.IsTrue(draftpage.CheckDraftContent(letter.Addressee, letter.Body), "Error in draft content");
             Assert.IsTrue(draftpage.SendEmail(), "Error while sending email");
             homepage.GoToDraftsFolder();
             Assert.IsTrue(draftpage.IsElementPresent(By.XPath("//div[@class='b-datalist__empty__block']")), "Email was not sent");
             SentPage sentpage = homepage.GoToSentPage();
-            Assert.IsTrue(sentpage.CheckEmailSent(bodyEmail), "Sent folder is empty, no email was sent");
+            Assert.IsTrue(sentpage.CheckEmailSent(letter.Body), "Sent folder is empty, no email was sent");
             homepage.LogOff();
             Assert.IsTrue(loginpage.LoggedOut(), "Log off failed");
             driver.Quit();
